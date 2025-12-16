0bf9064b896b80699a6fbded38d68ce1fa3f4f16
Vinicius <93095246+vmoreira-dev@users.noreply.github.com>
2025-12-16 10:27:39 -0500

    reverted back to normal

--- src/app/components/FeaturedProjects.tsx
+++ src/app/components/FeaturedProjects.tsx
@@ -1,27 +1,17 @@
 // app/components/FeaturedProjects.tsx
 import { projects } from "../_data/projects";
+
 import ProjectCard from "./ProjectCard";
 
 export default function FeaturedProjects() {
   return (
     <section
       id="projects"
-      className="mt-40 pb-48 flex flex-col gap-20"
+      className="mt-32 pb-40 flex flex-col gap-32"
     >
-          <h2
-        className="
-          text-center
-          text-xl md:text-2xl
-          font-semibold
-          tracking-tight
-          text-slate-900
-          transition-all duration-300
-          hover:text-slate-950
-        "
-      >
-        Selected Work
+      <h2 className="text-center text-2xl font-semibold tracking-tight">
+        Featured Projects
       </h2>
-
 
       {projects.map((project) => (
         <ProjectCard
