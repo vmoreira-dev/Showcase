6f6025fdd97f1dab967b97d303e02915fa579802
Vinicius <93095246+vmoreira-dev@users.noreply.github.com>
2025-12-17 10:32:59 -0500

    changed title and added space

--- src/app/components/FeaturedProjects.tsx
+++ src/app/components/FeaturedProjects.tsx
@@ -1,17 +1,25 @@
 // app/components/FeaturedProjects.tsx
 import { projects } from "../_data/projects";
-
 import ProjectCard from "./ProjectCard";
 
 export default function FeaturedProjects() {
   return (
     <section
       id="projects"
-      className="mt-32 pb-40 flex flex-col gap-32"
+      className="
+        scroll-mt-32
+        pt-30
+        pb-40
+        flex flex-col
+        gap-24
+      "
     >
-      <h2 className="text-center text-2xl font-semibold tracking-tight">
-        Featured Projects
-      </h2>
+      <div className="flex flex-col items-center gap-6">
+        <h2 className="text-3xl md:text-4xl font-semibold tracking-tight text-slate-900">
+          Selected Work
+        </h2>
+        <span className="h-px w-12 bg-black/10" />
+      </div>
 
       {projects.map((project) => (
         <ProjectCard
